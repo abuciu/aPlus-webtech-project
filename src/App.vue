a6022a41b813e65353fa8aadef4baefed2aa3e1f
andrei <andrei@buciu.xyz>
2021-11-18 23:57:44 +0200

    Test Vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/a-plus.png">
+  <HelloWorld msg="Anonimous grading by Web Gladiators"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+}
+</style>
