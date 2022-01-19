7741cc2ba28fcd3376d2e150f2203d5157104b89
andrei <andrei@buciu.xyz>
2022-01-19 22:44:35 +0200

    final version after long hours of coding the couple of last nights and today

--- server.js
+++ server.js
@@ -5,41 +5,10 @@ const sqlite = require('sqlite3')
 const cors=require('cors')
 
 
-//Creare instanta baza de date sqlite folosind sequelize
+//Se creeaza o instanta sequelize ce foloseste baza de date creata de noi
 const sequelize = new Sequelize({dialect: 'sqlite' ,storage: './aPlus.sqlite'})
 
-//Creare tabela proiecte
-const Project = sequelize.define('project', {
-    fileLink: {
-        type: Sequelize.STRING,
-        allowNull: true
-    },
-    version: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    title: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    deadline: {
-        type: Sequelize.DATE,
-        allowNull: false
-    },
-    final_grade: {
-        type: Sequelize.DOUBLE,
-        allowNull: true
-    },
-    personID: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    }
-},
-    {
-        timestamps: false
-    })
-
-//Creare tabela persoane
+//Se creeaza tabela persoane si se definesc campurile aferente
 const Person = sequelize.define('person', {
     type: {
         type: Sequelize.ENUM,
@@ -71,7 +40,38 @@ const Person = sequelize.define('person', {
         timestamps: false
     })
     
-//Creare tabela note
+//Se creeaza tabela proiecte si se definesc campurile aferente
+const Project = sequelize.define('project', {
+    fileLink: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    version: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    deadline: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    final_grade: {
+        type: Sequelize.DOUBLE,
+        allowNull: true
+    },
+    personID: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+},
+    {
+        timestamps: false
+    })
+
+//Se creeaza tabela note si se definesc campurile aferente
 const Grade = sequelize.define('grade', {
     projectTitle: {
         type: Sequelize.STRING,
@@ -96,7 +96,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-//Acest get request va crea tabelele in caz ca nu exista
+//Se creaza tabelele definite anterior in caz ca acestea nu exista
 app.get('/create', async (req, res, next) => {
     try {
         await sequelize.sync({ alter: true })
@@ -106,7 +106,7 @@ app.get('/create', async (req, res, next) => {
     }
 })
 
-//Acest post request adauga o intrare in tabela persoane
+//Se populeaza o intrare in tabela de persoane
 app.post('/person', async (req, res, next) => {
     try {
         await Person.create(req.body)
@@ -116,7 +116,7 @@ app.post('/person', async (req, res, next) => {
     }
 })
 
-//Acest get request returneaza toate persoanele din tabela
+//Se obtine lista de persoane
 app.get('/people', async (req, res, next) => {
     try {
         const people = await Person.findAll()
@@ -126,11 +126,86 @@ app.get('/people', async (req, res, next) => {
     }
 })
 
-//Toate erorile returneaza acest mesaj cu status code 500
+//Se populeaza o intrare in tabela proiecte
+app.post('/projects', async (req, res, next) => {
+    try {
+        await Project.create(req.body)
+        res.status(201).json({ message: 'created' })
+    } catch (err) {
+        next(err)
+    }
+})
+
+//Se actualizeaza datele unei intrari din tabela de proiecte
+app.put('/projects/:id/:version/:personID', async (req, res, next) => {
+    try {
+        let projID = req.params.id
+        let versionID = req.params.version
+        let persID = req.params.personID
+        await Project.update(req.body, {
+            where: {
+                id: projID,
+                version: versionID,
+                personID: persID
+            }
+        })
+        res.status(200).json({ message: "updated" })
+    } catch (err) {
+        next(err)
+    }
+})
+
+//Se obtine lista de proiecte
+app.get('/projects', async (req, res, next) => {
+    try {
+        const projects = await Project.findAll()
+        res.status(200).json(projects)
+    } catch (err) {
+        next(err)
+    }
+})
+
+//Se obtine lista de proiecte pentru o anumita persoana
+app.get('/projects/:personID', async (req, res, next) => {
+    try {
+        let persID = req.params.personID
+        const projects = await Project.findAll({
+            where:
+            {
+                personID: persID
+            }
+        })
+        res.status(200).json(projects)
+    } catch (err) {
+        next(err)
+    }
+})
+
+//Se populeaza o intrare in tabela de note
+app.post('/grades', async (req, res, next) => {
+    try {
+        await Grade.create(req.body)
+        res.status(201).json({ message: 'created' })
+    } catch (err) {
+        next(err)
+    }
+})
+
+//Se obtine lista de note
+app.get('/grades', async (req, res, next) => {
+    try {
+        const grades = await Grade.findAll()
+        res.status(200).json(grades)
+    } catch (err) {
+        next(err)
+    }
+})
+
+//Aici revin toate erorile
 app.use((err, req, res, next) => {
     console.warn(err)
     res.status(500).json({ message: 'server error' })
 })
 
-//Serverul asculta pe portul 8080
+//Am definit portul pe care asculta aplicatia
 app.listen(8080)
